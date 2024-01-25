f54af5ea9544c5830b2dda79b6af2e34cda0006b
Vincent <vdschilling@outlook.com>
2024-01-25 18:53:37 +0100

    add banner

---
+++ components/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="banner">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.banner {
+    background-color: #f5f5f5;
+    border: 1px solid #ebebeb;
+    border-radius: 4px;
+    padding: 20px;
+    margin-bottom: 20px;
+}
+</style>
